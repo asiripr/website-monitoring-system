4674546a169ab538901e59dc2600c6515529e4b4
asiripr <asiripramodaya2000@gmail.com>
2025-02-18 10:26:27 +0530

    declared the database and created the migration files

---
+++ website-monitoring-system-backend/database/migrations/2025_02_18_035130_create_websites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            // auto incrementing primary key
+            $table->id();
+            // user_id = id column of users table, and if a record of users table deleted -> it will removed the all related logs within this as well
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            // create url column with unique values
+            $table->string('url')->unique();
+            // status column, only 3 values, default is 'unknown'
+            $table->enum('status', ['up', 'down', 'unknown'])->default('unknown'); 
+            // last checked time
+            $table->timestamp('last_checked_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('websites');
+    }
+};
