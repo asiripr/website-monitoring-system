ae91035258bbf78007a194d68c33ea4a6028cdc1
asiripr <asiripramodaya2000@gmail.com>
2025-02-19 10:04:52 +0530

    setted up with sanctum

--- website-monitoring-system-backend/config/cors.php
+++ website-monitoring-system-backend/config/cors.php
@@ -15,11 +15,13 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    // 'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // this has been changed to integrated with react frontend based on sanctum
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['http://localhost:5173'], // Allow React frontend
 
     'allowed_origins_patterns' => [],
 
--- website-monitoring-system-backend/routes/api.php
+++ website-monitoring-system-backend/routes/api.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
+// User Registration
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+// User Login
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+// User logout -> reomve the session
+Route::middleware('auth:sanctum')->post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
